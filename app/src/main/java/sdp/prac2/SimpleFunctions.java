95227367508654da130ef6e64fbaf6e292eb8af1
Kwenza05 <g20h4776@campus.ru.ac.za>
2024-05-18 12:46:15 +0200

    Task 1 method added

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,13 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public static int task1(List<Integer> a, List<Integer> b) {
+        int sum = 0;
+        for (int i = 0; i < b.size(); i++) {
+            if (b.get(i) < a.size() && b.get(i) >= 0) {
+                sum += a.get(b.get(i));
+            }
+        }
+        return sum;
+    }
 }
