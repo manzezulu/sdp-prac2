92780365eacefcc8a359e9ba3e22487407b427b1
mdluli__ <akhonamdluli22@gmail.com>
2024-05-18 12:37:04 +0200

    Added Task 5

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+        // Method to check if a list is sorted
+        public static <T extends Comparable<T>> boolean isSorted(List<T> list) {
+            if (list == null || list.size() <= 1) { //If list is empty or has one element than sorted
+                return true; 
+            }
+            for (int i = 0; i < list.size() - 1; i++) { //If the current element is > than the next the list is not sorted
+                if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+                    return false; 
+                }
+            }
+            return true; //If list is in order
+    }
 }
