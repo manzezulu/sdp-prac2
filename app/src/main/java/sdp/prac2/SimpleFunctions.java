14104b9d9d7c2dd6ebda183e2da4432c9c12c32e
manzezulu <g20m7935@campus.ru.ac.za>
2024-05-18 22:00:43 +0200

    task 1 added to main file

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,6 +4,15 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+        public static int task1(List<Integer> a, List<Integer> b) {
+            int sum = 0;
+            for (int i = 0; i < b.size(); i++) {
+                if (b.get(i) < a.size() && b.get(i) >= 0) {
+                    sum += a.get(b.get(i));
+                }
+            }
+            return sum;
+        }
         // Method to check if a list is sorted
         public static <T extends Comparable<T>> boolean task5(List<T> list) {
         if (list == null || list.size() <= 1) { //If list is empty or has one element than sorted
